3b131233a931864a80f9d4a25d39400b25458dbd
Djes Samuels <DJDjes13@gmail.com>
2025-07-01 12:16:30 +0000

    Dit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation';
+
+export default function DashboardRootPage() {
+  redirect('/dashboard/general');
+  // This return is needed to satisfy TypeScript, but the redirect will happen before it's ever rendered.
+  return null;
+}
