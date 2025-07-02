d45d9043ac85b6a2d2eabd9de5bfaf06eae0d037
Djes Samuels <DJDjes13@gmail.com>
2025-07-02 10:29:50 +0000

    Analyseer de opgegeven codebase grondig om de structuur te verbeteren do

--- next.config.js
+++ next.config.js
@@ -8,12 +8,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
